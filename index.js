094a8aa1418e1ff49dd25f103a995589f6ffbb00
别云间 <oncwnuKKv1QEBseImsAvosy4rxpk@git.weixin.qq.com>
2019-05-11 15:49:17 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+function onConnection(socket){
+  console.log("建立连接成功");
+  socket.on('drawing', (data) => {
+    socket.broadcast.emit('drawing', data);
+    console.log(data)
+  });
+}
+
+io.on('connection', onConnection);
+
+http.listen(port, () => console.log('listening on port ' + port));
